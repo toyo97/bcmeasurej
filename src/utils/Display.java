b4a363aa005a0cb13d9cc2e9dcb7f311cc6c83e0
Vittorio <zampinetti@gmail.com>
2019-09-02 20:11:36 +0200

    Implemented circleRoi and applyLUT methods

---
+++ src/utils/Display.java
@@ -0,0 +1,41 @@
+package utils;
+
+import ij.IJ;
+import ij.gui.OvalRoi;
+import ij.gui.Roi;
+import ij.plugin.LutLoader;
+import ij.process.LUT;
+import ij.process.StackStatistics;
+
+import java.awt.*;
+import java.awt.image.IndexColorModel;
+import java.io.IOException;
+
+import stack.CellStack;
+
+
+public class Display {
+
+    public static void applyLUT(CellStack cellStack, String colorMap) throws IOException {
+        StackStatistics stats = new StackStatistics(cellStack);
+
+        if (colorMap.equals("fire")) {
+
+            IndexColorModel colorModel = LutLoader.open("luts/fire.lut");
+
+            LUT lut = new LUT(colorModel, stats.min, stats.max);
+            cellStack.setLut(lut);
+        }
+        else
+            IJ.error("Invalid color map " + colorMap + "\nDefault LUT applied");
+    }
+
+    public static void circleRoi(CellStack cellStack, int r, Color color) {
+        int d = r * 2;
+        int x = cellStack.getCellCenter()[0] - r;
+        int y = cellStack.getCellCenter()[1] - r;
+        Roi roi = new OvalRoi(x, y, d, d);
+        roi.setStrokeColor(color);
+        cellStack.setRoi(roi);
+    }
+}
