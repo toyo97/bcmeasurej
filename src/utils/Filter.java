7359dd86f48b5d9077b8d4c1b3a4c0ce6b6b8be0
Vittorio <zampinetti@gmail.com>
2019-09-02 20:12:07 +0200

    Implemented gaussian, median and mean filters

---
+++ src/utils/Filter.java
@@ -0,0 +1,47 @@
+package utils;
+
+import ij.IJ;
+import ij.ImageStack;
+import ij.plugin.Filters3D;
+import ij.plugin.GaussianBlur3D;
+import stack.CellStack;
+
+public class Filter {
+
+    private static void gaussianIJ(CellStack cellStack, float sigma) {
+        double sigmaZ = cellStack.getScaleZ() * sigma;
+        GaussianBlur3D.blur(cellStack, sigma, sigma, sigmaZ);
+    }
+
+    private static void medianIJ(CellStack cellStack, float sigma) {
+        ImageStack stack = cellStack.getImageStack();
+        float sigmaZ = (float) cellStack.getScaleZ() * sigma;
+        ImageStack newStack = Filters3D.filter(stack, Filters3D.MEDIAN, sigma, sigma, sigmaZ);
+
+        cellStack.setStack(newStack);
+    }
+
+    private static void meanIJ(CellStack cellStack, float sigma) {
+        ImageStack stack = cellStack.getImageStack();
+        float sigmaZ = (float) cellStack.getScaleZ() * sigma;
+        ImageStack newStack = Filters3D.filter(stack, Filters3D.MEAN, sigma, sigma, sigmaZ);
+
+        cellStack.setStack(newStack);
+    }
+
+    public static void filterCellStack(CellStack cellStack, String method, float sigma) {
+        switch (method) {
+            case "gauss":
+                gaussianIJ(cellStack, sigma);
+                break;
+            case "mean":
+                meanIJ(cellStack, sigma);
+                break;
+            case "median":
+                medianIJ(cellStack, sigma);
+                break;
+            default:
+                IJ.error("Filter " + method + " not valid\nImage not filtered");
+        }
+    }
+}
