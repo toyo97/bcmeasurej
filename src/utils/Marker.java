0f02759c93b5b9b81afa5b91cafb9673976626cf
Vittorio <zampinetti@gmail.com>
2019-09-04 19:42:07 +0200

    Write method

--- src/utils/Marker.java
+++ src/utils/Marker.java
@@ -1,10 +1,13 @@
 package utils;
 
 import ij.IJ;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Marker {
 
@@ -12,6 +15,15 @@ public class Marker {
         return Integer.toString(height - (int) Float.parseFloat(y));
     }
 
+
+    /**
+     * Extract the seeds coordinates from a csv file
+     *
+     * @param markerPath absolute path of the .marker file
+     * @param imgHeight  height of the image if y coordinates inversion is requested. If 0 or less is passed, no inversion is done
+     * @return rows of the .marker file coordinates casted to int values
+     * @throws IOException when no marker file is found
+     */
     public static ArrayList<int[]> readMarker(String markerPath, int imgHeight) throws IOException {
 
         ArrayList<int[]> rows = new ArrayList<>();
@@ -41,7 +53,29 @@ public class Marker {
         return rows;
     }
 
+    /**
+     * Same as above but when no y inversion is needed
+     */
     public static ArrayList<int[]> readMarker(String markerPath) throws IOException {
         return Marker.readMarker(markerPath, 0);
+    }
+
+    public static void writeMarker(String markerPath, List<List<String>> data) {
+        try {
+            FileWriter csvWriter = new FileWriter(markerPath);
+            csvWriter.append("x,y,z,r").append("\n");
+
+            for (List<String> row: data){
+                csvWriter.append(String.join(",", row));
+                csvWriter.append("\n");
+            }
+
+            csvWriter.flush();
+            csvWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            IJ.log("No file written: " + e.getMessage());
+        }
+
     }
 }
